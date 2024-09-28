97c1b5a3f3458e6ef36798617597b1841950b8b1
GauravJangidRam <gaurav14jangid@gmail.com>
2024-09-29 00:32:17 +0530

    learing middle ware and routing ,request and response or error headling

--- express/scrpit.js
+++ express/scrpit.js
@@ -1,32 +1,58 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
 
-const http = require('http');
-const emoji = require('node-emoji');
+// const http = require('http');
+// const emoji = require('node-emoji');
 
-// Define the port to listen on
-const port = 3000;
+// // Define the port to listen on
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-    // Use the emoji package to print an emoji
-    const message = `Hello World ${emoji.get('earth_americas')} ${emoji.get('smile')}`;
+// const server = http.createServer((req, res) => {
+//     // Use the emoji package to print an emoji
+//     const message = `Hello World ${emoji.get('earth_americas')} ${emoji.get('smile')}`;
 
-    // Send response with the emoji message
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(message);
+//     // Send response with the emoji message
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end(message);
 
-    // Also log the message to the console
-    console.log(message);
-});
+//     // Also log the message to the console
+//     console.log(message);
+// });
 
-// Start the server and listen on the specified port
-server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
-});
+// // Start the server and listen on the specified port
+// server.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}/`);
+// });
 
-// app.listen(3000)
+// // app.listen(3000)
+
+
+
+
+const express = require('express');
+const apps = express();
+apps.use(function(req,res,next){
+  console.log('Request received');
+  next();
+ });
+
+ apps.get('/profie',(req,res,next)=>{
+  //  res.send('Hello express ');
+   return next(new Error('somthing wrong'));
+  });
+  
+  
+  apps.use(function(err, req, res, next){
+   console.error(err.stack);
+   res.status(500).send('Something broke!');
+  })
+
+ apps.listen('3000');
+
+//  Error heading 
+
