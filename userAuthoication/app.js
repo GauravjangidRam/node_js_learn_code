463d2ba2fc4680cec0f8059b8267d530856f2f96
GauravJangidRam <gaurav14jangid@gmail.com>
2024-10-15 23:44:54 +0530

    working on try what we learing partices

--- userAuthoication/app.js
+++ userAuthoication/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser'); // Required to work with cookies
 const app = express();
 const port = 3000;
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(cookieParser()); // Added middleware to parse cookies
 app.set('view engine', 'ejs');
 
 // MongoDB Connection
@@ -38,7 +40,6 @@ const userSchema = new mongoose.Schema({
 });
 
 // User model 
-
 const User = mongoose.model('User', userSchema);
 
 // Routes
@@ -60,8 +61,10 @@ app.post('/register', async (req, res) => {
       username,
       password: hashedPassword
     });
-    let token = jwt.sign({email},"sercet");
-    res.cookie("token",token);
+
+    // Use a secret key when signing the JWT
+    let token = jwt.sign({ email }, "yourSecretKey"); // Replace "yourSecretKey" with your actual secret key
+    res.cookie("token", token);
     await user.save();
     res.redirect('/');
   } catch (err) {
@@ -69,25 +72,30 @@ app.post('/register', async (req, res) => {
     res.status(500).send('Error registering user');
   }
 });
-app.post('/register',(req,res)=>{
-  let user = User.findOne({email: req.body.email});
-  if(!user){
-    return res.status(401).send('Invalid email');
-  }else{
-    bcrypt.compare(req.body.password,user.password,(err,result)=>{
-      if(err) throw err;
-      if(result){
-        let token = jwt.sign({email: user.email});
-        res.cookie("token",token);
-        res.redirect('/dashboard');
-      }
-      else{
-        return res.status(401).send('SomeThing is wrong');
-      }
 
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', async (req, res) => {
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return res.status(401).send('Invalid email');
+  } else {
+    bcrypt.compare(req.body.password, user.password, (err, result) => {
+      if (err) throw err;
+      if (result) {
+        // Use a secret key when signing the JWT
+        let token = jwt.sign({ email: user.email }, "yourSecretKey"); // Replace "yourSecretKey" with your actual secret key
+        res.cookie("token", token);
+        res.redirect('/');
+      } else {
+        return res.status(401).send('Incorrect password');
+      }
     });
   }
-})
+});
+
 app.get('/logout', (req, res) => {
   // Clear the cookie by setting its expiration to a past date
   res.cookie('token', '');
